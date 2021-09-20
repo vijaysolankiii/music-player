76ba3804c8676bb68c9fa86d3fba1d1024a6dd71
vijaysolanki <vijaysolanki.cnc@gmail.com>
2021-09-20 17:46:16 +0530

    initial commit

---
+++ music/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+    path('', include("App.urls")),
+    path('admin/', admin.site.urls),
+
+
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
